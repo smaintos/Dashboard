6a2fad3e0a71f6a9241574b93d1925d9fe152783
smaintos <william.mariamon@ynov.com>
2024-04-10 17:24:49 +0200

    up save 1

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')  # Utiliser un backend non interactif
+
 from flask import Flask, render_template
 import requests
 import base64
 from io import BytesIO
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt  # Ajoutez cette ligne pour importer plt
 
 app = Flask(__name__)
 
@@ -16,42 +18,43 @@ def get_api_data():
     else:
         return None
 
-def create_pie_chart(games):
-    fig = Figure()
-    ax = fig.subplots()
-    
-    # Préparation des données pour le graphique
-    # Supposons que vous voulez afficher le nombre de joueurs en ligne pour chaque jeu
-    # Vous pouvez ajuster cela en fonction des données réelles que vous souhaitez afficher
+def create_custom_chart(games):
+    # Utiliser les données de l'API pour générer le graphique
     values = [game['current_players'] for game in games]
     labels = [game['name'] for game in games]
     
-    # Génération des couleurs pour chaque segment du graphique
-    colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(values)))
+    # Créer le graphique
+    fig, ax = plt.subplots()
     
-    # Création du graphique de type pie chart
-    ax.pie(values, labels=labels, colors=colors, radius=3, center=(4, 4),
-           wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True, autopct='%1.1f%%')
+    # Créer un bar chart basé sur les données de l'API
+    ax.bar(labels, values)
     
-    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-           ylim=(0, 8), yticks=np.arange(1, 8))
+    ax.set(xlim=(0, len(labels)), xticks=np.arange(len(labels)),
+           ylim=(0, max(values)*1.2))
     
+    # Ajouter les labels
+    ax.set_xticklabels(labels, rotation=45, ha="right")
+    
+    # Ajouter les valeurs au-dessus des barres
+    for i, v in enumerate(values):
+        ax.text(i, v + max(values)*0.01, str(v), ha='center', va='bottom')
+    
+    # Sauvegarder le graphique en tant qu'image PNG
     buf = BytesIO()
-    fig.savefig(buf, format="png")
+    fig.savefig(buf, format="png", bbox_inches="tight")
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    plt.close(fig)  # Fermer la figure pour libérer la mémoire
     return f"data:image/png;base64,{data}"
-
 
 @app.route('/')
 def index():
     api_data = get_api_data()
     if api_data:
         games = api_data
-        pie_chart_data = create_pie_chart(games)
-        return render_template('index.html', games=games, pie_chart_data=pie_chart_data)
+        custom_chart_data = create_custom_chart(games)
+        return render_template('index.html', games=games, custom_chart_data=custom_chart_data)
     else:
         return "Erreur lors de la récupération des données de l'API"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
